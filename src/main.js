3c8b557a5d5a7c48b81ef7676ccf47278d4c017e
Eldin van Hengst <enr.vanhengst@live.nl>
2020-10-21 19:09:55 +0200

    first

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import DefaultLayout from "~/layouts/Default.vue";
 import settings from "../data/theme.json";
+import BootstrapVue from 'bootstrap-vue'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import "@/assets/code-highlight.css"
+import '~/assets/styles.css'
+
 
 export default function(Vue, { head }) {
   Vue.component("Layout", DefaultLayout);
+  Vue.use(BootstrapVue);
   head.bodyAttrs = {
     class: settings.dark_mode ? "dark" : ""
   };
